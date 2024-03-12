641960926427887264ab2c285db15571013170da
Jan Lehnardt <jan@apache.org>
2024-03-12 13:48:24 +0100

    remove more debug logs

--- services/kafka.ts
+++ services/kafka.ts
@@ -8,10 +8,7 @@ let kafka: Kafka
  * Returns a singleton instance of the Kafka manager from the
  * container
  */
-console.log('a-k services main app', app)
-
 await app.booted(async () => {
-  console.log('a-k services main app booted')
   kafka = await app.container.make('kafka')
 })
 
